412353de6214945f1024aea03dfd97c9c4e9db4a
Afonso Carpinteiro <32978324+Carpas444@users.noreply.github.com>
2024-10-28 22:37:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+print("---------------------------------") 
+
+print("|                               |") 
+
+print("|   The Coder Coffee Shop       |") 
+
+print("|         Bem-vindo!            |") 
+
+print("|                               |") 
+
+print("---------------------------------") 
+
+print("") 
+
+#Nesta primeira fase, devemos apresentar ao cliente os tipos de cafés que servimos 
+
+print("Estes são os nossos tipos de café:") 
+
+print(" > Espresso") 
+
+print(" > Americano") 
+
+print(" > Latte") 
+
+print(" > Cappuccino") 
+
+print(" > Macchiato") 
+
+print(" > Mocha") 
+
+print("----------------------------") 
+
+preco = 0  
+
+#Aqui perguntamos ao cliente qual o tipo de café escolhido. 
+
+#Tanto o menu de opções como a parte onde se pergunta ao
+#cliente o que deseja devem estar presentes em todas as fases!
+
+coffee = input("Que tipo de café é que pretende?") 
+
+if coffee=="Espresso": 
+
+   preco = preco + 2.50 
+
+elif coffee=="Americano": 
+
+   #Completa o resto do código daqui para baixo 
+
+   #Lembra-te que deves concluir, no mínimo, as três fases de atendimento descritas no enunciado! 
